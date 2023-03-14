f87d34be0d09585ce2b3638857b7085dd1129df0
Andrew Ngo <ango@future.fit>
2023-03-14 12:23:06 -0500

    Font weight increase on top user stats

--- src/components/leaderboard/top/TopUserStat.tsx
+++ src/components/leaderboard/top/TopUserStat.tsx
@@ -8,8 +8,8 @@ interface ITopUserStatProps {
 export const TopUserStat: FC<ITopUserStatProps> = ({ value, label }) => {
   return (
     <div>
-      <div className="leading-none tablet:text-24">{value}</div>
-      <div className="text-8 uppercase text-white/30 tablet:text-12">{label}</div>
+      <div className="font-medium leading-none tablet:text-24">{value}</div>
+      <div className="text-8 font-medium uppercase text-white/30 tablet:text-12">{label}</div>
     </div>
   )
 }
